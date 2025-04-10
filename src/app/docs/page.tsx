44fc271a13baa5ae66651d7b24cefc42de41956b
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-04-10 16:58:27 +0700

    add docs frame

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DocsPage = () => {
+  return (
+    <div>
+        <title>Do Not Disclose – Documentation</title>
+        <iframe src="https://dhanavadh.notion.site/ebd/1d155186ce448098948ada4bff34763f" className='frame' allowFullScreen />
+    </div>
+  )
+}
+
+export default DocsPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import { Kanit } from 'next/font/google'
+ 
+const kanit = Kanit({
+  weight: '400',
+  subsets: ['latin'],
+})
 export const metadata: Metadata = {
   title: "iconroof – ระแนงบังตา",
   description: "iconroof – ระแนงบังตา",
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={kanit.className}
       >
         {children}
       </body>
